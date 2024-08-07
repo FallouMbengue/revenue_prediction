b0046f976d4adc322500e369ddb1710e69f0c0cb
MorNdour <ndmor@ept.sn>
2024-08-07 14:33:52 +0000

    an other version

--- settings/params.py
+++ settings/params.py
@@ -13,11 +13,11 @@ MODEL_PARAMS = {
     "MIN_PPS":0.01,
     "TEST_SIZE": 0.2,
     "DEFAULT_FEATURE_NAMES":[
-       'location', 'cuisine', 'rating', 'seating capacity',
-       'average meal price', 'marketing budget', 'social media followers',
-       'chef experience years', 'number of reviews', 'avg review length',
-       'ambience score', 'service quality score', 'parking availability',
-       'weekend reservations', 'weekday reservations', 
+       'city', 'city group', 'type', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6',
+       'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'p14', 'p15', 'p16',
+       'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24', 'p25', 'p26',
+       'p27', 'p28', 'p29', 'p30', 'p31', 'p32', 'p33', 'p34', 'p35', 'p36',
+       'p37', 'day', 'day_name', 'month', 'years'
     ]
     
 }
